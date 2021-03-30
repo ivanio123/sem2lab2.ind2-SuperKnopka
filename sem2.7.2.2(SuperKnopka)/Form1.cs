592626574535238e9b9dd1513ec4c885da686b85
Ivan Dzherikhov <jodsonjod@gmail.com>
2021-03-30 19:51:13 +0300

    Добавьте файлы проекта.

---
+++ sem2.7.2.2(SuperKnopka)/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sem2._7._2._2_SuperKnopka_
+{
+    public partial class Form1 : Form
+    {
+        delegate void DelegateButton(object sender, EventArgs e);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1f)
+            {
+                this.Opacity = 1f;
+            }
+            else
+            {
+                this.Opacity = 0.5f;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == SystemColors.ActiveCaption)
+                this.BackColor = Color.OldLace; 
+            else 
+                this.BackColor = SystemColors.ActiveCaption; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я супермегакнопка,\nі цього мене не позбавиш!");
+            DelegateButton[] buttons = new DelegateButton[] { button1_Click, button2_Click, button3_Click };
+            if (checkBox1.Checked == true)
+            {
+                buttons[0](sender, e);
+            }
+            if (checkBox2.Checked == true)
+            {
+                buttons[1](sender, e);
+            }
+            if (checkBox3.Checked == true)
+            {
+                buttons[2](sender, e);
+            }
+        }
+    }
+}
---
+++ sem2.7.2.2(SuperKnopka)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("sem2.7.2.2(SuperKnopka)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sem2.7.2.2(SuperKnopka)")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c010a3c2-a1b5-444a-84f2-b3362cd2ba27")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
